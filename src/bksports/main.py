ff64be176efe89baa13412dfbbb0c33d545a93d8
bk24z <bradsthedeveloper@gmail.com>
2026-02-04 15:22:18 +0000

    Reformat main.py, add pygame.quit()

--- src/bksports/main.py
+++ src/bksports/main.py
@@ -1,19 +1,21 @@
 import pygame
 
-from bksports.constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from bksports.bowling.game import BowlingGame
+from bksports.constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
-def main():
+
+def main() -> None:
     running = True
     while running:
         bowling_game = BowlingGame(screen, clock)
         bowling_game.run()
         running = bowling_game.running
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
